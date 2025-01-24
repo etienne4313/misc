85aa7a62cc6b8dced93783adc5db31a01c17a1bf
Etienne <etienne4313@gmail.com>
2025-01-24 07:43:40 -0500

    Adding vm ssh networking
    Signed-off-by: Etienne <etienne4313@gmail.com>

--- vm/vm_run.sh
+++ vm/vm_run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# ssh localhost -p1810
 
 /usr/bin/qemu-system-x86_64 \
   -machine q35 \
@@ -8,6 +9,7 @@
   -chardev socket,id=vserial0,host=127.0.0.1,port=50000,telnet,server,nowait \
   -device virtio-serial -device virtconsole,chardev=vserial1 \
   -chardev socket,id=vserial1,host=127.0.0.1,port=50001,telnet,server,nowait \
+  -net nic -net user,hostfwd=tcp::1810-:22 \
   -m 4G \
   -enable-kvm \
   -smp 4 \
